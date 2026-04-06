139891eb2c2d6dbd5396a26858d47bd6775c157d
Hrishikesh Giri <hrishikgiri@gmai.com>
2026-04-06 00:12:45 -0700

    Minor fixes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,7 +6,6 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  console.log('Interceptor firing, token:', token?.slice(0, 20));
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
