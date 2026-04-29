cf5d81801dcdd0b73a4897e79c12dfae4edbcee1
Rithika SridharBabu <rithikasridharbabu22@gmail.com>
2026-04-29 12:38:17 -0700

    Add forgot password and reset password feature

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  resetToken: {
+    type: String
+  },
+  resetTokenExpiry: {
+    type: Date
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
